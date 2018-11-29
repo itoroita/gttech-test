757c410821a48b1ea91ad027453700dfcb2581c1
itoroita <45194304+itoroita@users.noreply.github.com>
2018-11-29 10:26:44 +0100

    itoro-ita test

---
+++ itoro-ita/ex1.py
@@ -0,0 +1,13 @@
+# create a function that takes in a string as an input and repeats that string in 3 places
+# e.g. boy becomes boyboy.
+# note that you are to do a string multiplication
+
+def stringMultiple():
+
+    name = input('Enter the name: ')
+
+    output = name * 3;
+
+    print(output);
+
+stringMultiple();
---
+++ itoro-ita/ex3.py
@@ -0,0 +1,19 @@
+# write a function that takes your name
+# and returns the first and the last characters of your name
+# e.g. Chukwudi becomes Ci
+
+def name_shorting():
+
+    myName = input('Enter the name: ')
+
+    print(myName[0] + myName[4]);
+
+name_shorting()
+
+
+
+
+
+
+
+
